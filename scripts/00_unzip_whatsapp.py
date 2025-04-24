fbdf90dfa17a5a087cdd209e8ab9286f85a703f6
Harold <harold.gallice@gmail.com>
2025-04-24 14:25:14 +0200

    [HG] reorganize repo

--- scripts/00_unzip_whatsapp.py
+++ scripts/00_unzip_whatsapp.py
@@ -1,16 +1,21 @@
+"""
+Unzip the whatsapp exports
+"""
+
 import zipfile
 from os import path, listdir
 from pathlib import Path
 
 
 DATA_DIR = path.dirname(path.realpath(__file__))
-WHATSAPP_ZIP_DIR = path.join(DATA_DIR, "whatsapp_exports")
-WHATSAPP_TXT_DIR = path.join(DATA_DIR, "raw")
+WHATSAPP_ZIP_DIR = path.join(DATA_DIR, "../data/whatsapp_exports")
+WHATSAPP_TXT_DIR = path.join(DATA_DIR, "../data/raw")
 
 def extract_whatsapp_zip(zip_file_path):
     file_name = Path(zip_file_path).stem
     with zipfile.ZipFile(zip_file_path, "r") as zip_file:
         zip_file.extractall(path=path.join(WHATSAPP_TXT_DIR, file_name) )
+    print(f"Extracted {zip_file_path} to {file_name}/")
 
 def get_whatsapp_zip_files():
     return [f for f in listdir(WHATSAPP_ZIP_DIR) if f.endswith(".zip")]
